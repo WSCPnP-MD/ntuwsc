2f4d3c472129fc1a60ba0f1dc9e78107acf13660
wongyuanneng <wongyuanneng@gmail.com>
2018-12-13 23:45:35 +0800

    sponsor page draft1

---
+++ page-159.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Page Template
+ *
+ * @package Page Builder Framework
+ */
+ 
+// exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+get_header(); ?>
+
+
+	<main>
+        <div class="portfolio sponsor">
+            <h2>Welfare Services Club (WSC)</h2>
+                <ul>
+                    <li>National Council of Social Service (NCSS)</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NCSSlogo.jpg">
+                    <li>WSC Alumni Association</li>
+                    <li>People's Association Youth Movement (PAYM)</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/PAYM.jpg">
+                </ul>
+            <h2>Publicity and Publications (PnP)</h2>
+                <ul>
+                    <li>National Youth Council</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NYCS.jpg">
+                </ul>
+            <h2>RSP (Elders)</h2>
+                <ul>
+                    <li>Peach Garden Group</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/peach.jpg">
+                    <li>Unipack Pte Ltd</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/unipack.jpg">
+                    <li>ITRADE</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/itrade.jpg">
+                    <li>Universal Sovereign Trading Pte Ltd</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/pursoft.jpg">
+                    <li>Borden Company Pre Ltd</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/borden.jpg">
+                    <li>UIC Consumer Products Pte Ltd</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/uic.jpg">
+                </ul>
+            <h2>RSP (Friends of Children)</h2>
+                <ul>
+                    <li>National Youth Council</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NYCS.jpg">
+                    <li>People's Association</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/PA.jpg">
+                    <li>Jurong Point Management Office</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/jp.jpg">
+                </ul>
+            <h2>RSP (Visually Handicapped)</h2>
+            <h2>RSP (Hearing Impaired)</h2>
+                <ul>
+                    <li>National Youth Council</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NYCS.jpg">
+                </ul>
+            <h2>RSP (Physically Challenged)</h2>
+            <h2>RSP (Intellectually disabled)</h2>
+            <h2>RSP (Youth)</h2>
+                <ul>
+                    <li>Ministry of Social and Family Development (MSF)</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/msf.jpg">
+                    <li>Brothers of St Gabriel's</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/stgab.jpg">
+                    <li>Singapore Sailing Federation</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/SSF.jpg">
+                    <li>British Patroleum (BP)</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/BP.jpg">
+                </ul>
+            <h2>Ignite Change (IgC)</h2>
+            <h2>Camp OutReach (COR)</h2>
+                <ul>
+                    <li>National Youth Council</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NYCS.jpg">
+                    <li>City Development Limited</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/CDL.jpg">
+                    <li>Lee Foundation</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/lee.jpg">
+                    <li>Prudential</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/pru.jpg">
+                </ul>
+            <h2>Volunteer Management (VM)</h2>
+                <ul>
+                    <li>National Youth Council</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NYCS.jpg">
+                    <li>People's Association</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/PA.jpg">
+                </ul>
+            <h2>Overseas Volunteering Expedition (OVE)</h2>
+                <ul>
+                    <li>National Youth Council</li>
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NYCS.jpg">
+                </ul>
+        </div>
+	</main>    
+<?php get_footer(); ?>
