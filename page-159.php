b33f4a9c90d7f4e2b39b15ed5d41a999b5ab226d
wongyuanneng <wongyuanneng@gmail.com>
2018-12-17 02:40:47 +0800

    draft3

--- page-159.php
+++ page-159.php
@@ -17,88 +17,126 @@ get_header(); ?>
                 width: 150px;
                 height: 90px;
             }
+            #part {
+                /* background-color: yellow; */
+                display: inline-block;
+                vertical-align: top;
+                padding-right: 5px;
+                border-right: 1px solid black;
+                height: 150px;
+                text-align: center;
+            }
+            p {
+                text-align: left;
+            }
         </style>
         <div class="portfolio sponsor" style="background-color: white;">
-            <h2>Welfare Services Club (WSC)</h2>
+            <div id='part'>
+                <p>Welfare Services Club (WSC)</p>
+    <!--                <ul>
+                        <li>National Council of Social Service (NCSS)</li> -->
+                            <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NCSSlogo.jpg">
+    <!--                    <li>WSC Alumni Association</li>
+                        <li>People's Association Youth Movement (PAYM)</li>-->
+                            <img src="../wp-content/themes/stand-out/assets/img/spons-logos/PAYM.jpg">
+    <!--                </ul> -->
+            </div>
+            <div id ='part'>
+                <p>Publicity and Publications (PnP)</p>
+    <!--                <ul>
+                        <li>National Youth Council</li> -->
+                            <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NYCS.jpg">
+    <!--                </ul> -->
+            </div>
+            <div id ='part'>
+                <p>RSP (Elders)</p>
+    <!--                <ul>
+                        <li>Peach Garden Group</li> -->
+                            <img src="../wp-content/themes/stand-out/assets/img/spons-logos/peach.jpg">
+    <!--                    <li>Unipack Pte Ltd</li> -->
+                            <img src="../wp-content/themes/stand-out/assets/img/spons-logos/unipack.jpg">
+    <!--                    <li>ITRADE</li> -->
+                            <img src="../wp-content/themes/stand-out/assets/img/spons-logos/itrade.jpg">
+    <!--                    <li>Universal Sovereign Trading Pte Ltd</li> -->
+                            <img src="../wp-content/themes/stand-out/assets/img/spons-logos/pursoft.jpg">
+    <!--                    <li>Borden Company Pre Ltd</li> -->
+                            <img src="../wp-content/themes/stand-out/assets/img/spons-logos/borden.jpg">
+    <!--                    <li>UIC Consumer Products Pte Ltd</li> -->
+                            <img src="../wp-content/themes/stand-out/assets/img/spons-logos/uic.jpg">
+    <!--                </ul> -->
+            </div>
+            <div id ='part'>
+                <p>RSP (Friends of Children)</p>
+    <!--               <ul>
+                        <li>National Youth Council</li> -->
+                            <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NYCS.jpg">
+    <!--                    <li>People's Association</li> -->
+                            <img src="../wp-content/themes/stand-out/assets/img/spons-logos/PA.jpg">
+    <!--                    <li>Jurong Point Management Office</li> -->
+                            <img src="../wp-content/themes/stand-out/assets/img/spons-logos/jp.jpg">
+    <!--                </ul> -->
+            </div>
+            <div id ='part'>
+                <p>RSP (Visually Handicapped)</p>
+            </div>
+            <div id ='part'>
+                <p>RSP (Hearing Impaired)</p>
+    <!--                <ul>
+                        <li>National Youth Council</li> -->
+                            <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NYCS.jpg">
+    <!--                </ul> -->
+            </div>
+            <div id ='part'>
+                <p>RSP (Physically Challenged)</p>
+            </div>
+            <div id ='part'>
+                <p>RSP (Intellectually disabled)</p>
+            </div>
+            <div id ='part'>
+                <p>RSP (Youth)</p>
 <!--                <ul>
-                    <li>National Council of Social Service (NCSS)</li> -->
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NCSSlogo.jpg">
-<!--                    <li>WSC Alumni Association</li>
-                    <li>People's Association Youth Movement (PAYM)</li>-->
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/PAYM.jpg">
-                </ul>
-            <h2>Publicity and Publications (PnP)</h2>
+                    <li>Ministry of Social and Family Development (MSF)</li> -->
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/msf.jpg">
+<!--                    <li>Brothers of St Gabriel's</li> -->
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/stgab.jpg">
+<!--                    <li>Singapore Sailing Federation</li> -->
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/SSF.jpg">
+<!--                    <li>British Patroleum (BP)</li> -->
+                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/BP.jpg">
+<!--                </ul> -->
+            </div>
+            <div id ='part'>
+                <p>Ignite Change (IgC)</p>
+            </div>
+            <div id ='part'>
+                <p>Camp OutReach (COR)</p>
 <!--                <ul>
                     <li>National Youth Council</li> -->
                         <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NYCS.jpg">
-                </ul>
-            <h2>RSP (Elders)</h2>
-<!--                <ul>
-                    <li>Peach Garden Group</li> -->
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/peach.jpg">
-<!--                    <li>Unipack Pte Ltd</li> -->
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/unipack.jpg">
-<!--                    <li>ITRADE</li> -->
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/itrade.jpg">
-<!--                    <li>Universal Sovereign Trading Pte Ltd</li> -->
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/pursoft.jpg">
-<!--                    <li>Borden Company Pre Ltd</li> -->
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/borden.jpg">
-<!--                    <li>UIC Consumer Products Pte Ltd</li> -->
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/uic.jpg">
-<!--                </ul> -->
-            <h2>RSP (Friends of Children)</h2>
-                <ul>
-                    <li>National Youth Council</li>
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NYCS.jpg">
-                    <li>People's Association</li>
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/PA.jpg">
-                    <li>Jurong Point Management Office</li>
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/jp.jpg">
-                </ul>
-            <h2>RSP (Visually Handicapped)</h2>
-            <h2>RSP (Hearing Impaired)</h2>
-                <ul>
-                    <li>National Youth Council</li>
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NYCS.jpg">
-                </ul>
-            <h2>RSP (Physically Challenged)</h2>
-            <h2>RSP (Intellectually disabled)</h2>
-            <h2>RSP (Youth)</h2>
-                <ul>
-                    <li>Ministry of Social and Family Development (MSF)</li>
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/msf.jpg">
-                    <li>Brothers of St Gabriel's</li>
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/stgab.jpg">
-                    <li>Singapore Sailing Federation</li>
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/SSF.jpg">
-                    <li>British Patroleum (BP)</li>
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/BP.jpg">
-                </ul>
-            <h2>Ignite Change (IgC)</h2>
-            <h2>Camp OutReach (COR)</h2>
-                <ul>
-                    <li>National Youth Council</li>
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NYCS.jpg">
-                    <li>City Development Limited</li>
+<!--                    <li>City Development Limited</li> -->
                         <img src="../wp-content/themes/stand-out/assets/img/spons-logos/CDL.jpg">
-                    <li>Lee Foundation</li>
+<!--                    <li>Lee Foundation</li> -->
                         <img src="../wp-content/themes/stand-out/assets/img/spons-logos/lee.jpg">
-                    <li>Prudential</li>
+<!--                    <li>Prudential</li> -->
                         <img src="../wp-content/themes/stand-out/assets/img/spons-logos/pru.jpg">
-                </ul>
-            <h2>Volunteer Management (VM)</h2>
-                <ul>
-                    <li>National Youth Council</li>
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NYCS.jpg">
-                    <li>People's Association</li>
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/PA.jpg">
-                </ul>
-            <h2>Overseas Volunteering Expedition (OVE)</h2>
-                <ul>
-                    <li>National Youth Council</li>
-                        <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NYCS.jpg">
-                </ul>
+<!--                </ul> -->
+            </div>
+            <div id ='part'>
+                <p>Volunteer Management (VM)</p>
+    <!--                <ul>
+                        <li>National Youth Council</li> -->
+                            <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NYCS.jpg">
+    <!--                    <li>People's Association</li> -->
+                            <img src="../wp-content/themes/stand-out/assets/img/spons-logos/PA.jpg">
+    <!--                </ul> -->
+            </div>
+            <div id ='part'>
+                <p>Overseas Volunteering Expedition (OVE)</p>
+    <!--                <ul>
+                        <li>National Youth Council</li> -->
+                            <img src="../wp-content/themes/stand-out/assets/img/spons-logos/NYCS.jpg">
+    <!--                </ul> -->
+            </div>
         </div>
 	</main>    
 <?php get_footer(); ?>
