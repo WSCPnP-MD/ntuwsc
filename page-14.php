db8a014a9bb656eb81f5cd8f0a44df59f92f9b3b
Tyea <spartashen@hotmail.com>
2018-12-21 11:13:58 +0800

    code clean up for php_about us

--- page-14.php
+++ page-14.php
@@ -10,117 +10,140 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 get_header(); ?>
 
+	<!--Main-->
 	<main>
+
+		<!--Child 0 background frame-->
 		<div class= "overlay">
 			<div class="overlayFN">
 
 			</div>
 		</div>
+
+		<!--Child 1 content frame-->
 		<div class="container">
 			<div class="row">
 				<div class="history">
+
+					<!--Sub 0_Text-->
 					<h2> Our History </h2>
+
 					<p class = "Hist_Head"> Welfare Service Club (WSC) had its humble beginnings in 1985 with a few portfolios and to date, we have 18 portfolios consisting of 
 					</p>
+
 					<ul class = "Hist_Sub">
 						<li> 9 Regular Service Projects (RSP) </li>
 						<li> 5 Special Projects (SP) </li>
 						<li> 4 Supporting Committees (SC) </li>
 					</ul>
-					<p class = "Hist_End" >
-						 We cover an extensive range of service users benefitting from our programmes - the Deaf, elderly, underprivileged children, the intellectually disabled, patients troubled with mental issues, the physically challenged, the visually handicapped, youths and low income families as well as the overseas communities in need. In addition, our programmes are also targeted at different groups in the society - mainly NTU students, the public and volunteers from schools or tertiary institutions. It is our hope to continually evaluate our programmes and expand them to stay relevant to the changing needs of our service users. 
-					</p>
-					<!--<img src="../wp-content/themes/stand-out/assets/img/wsc-timeline.jpg">-->
-					<h3>Our Timeline </h3>
 
+					<p class = "Hist_End"> We cover an extensive range of service users benefitting from our programmes - the Deaf, elderly, underprivileged children, the intellectually disabled, patients troubled with mental issues, the physically challenged, the visually handicapped, youths and low income families as well as the overseas communities in need. In addition, our programmes are also targeted at different groups in the society - mainly NTU students, the public and volunteers from schools or tertiary institutions. It is our hope to continually evaluate our programmes and expand them to stay relevant to the changing needs of our service users. 	 
+					</p>
+
+					<!--Sub 1_Text_Timeline structure-->
+					<h3>Our Timeline </h3>
 					<div id="content">
 
 						<ul class="timeline">
 							<li class="event" data-date="1985">
-							<h5>ID & PNP</h5>
-							<p  class = "TLP">"IDPNPLogo and Description"</p>
+								<h5>ID & PNP</h5>
+								<p  class = "TLP">"IDPNPLogo and Description"
+								</p>
 							</li>
 
 							<li class="event" data-date="1987">
-							<h5>Finance</h5>
-							<p class = "TLP">"FinanceLogo"</p>    
+								<h5>Finance</h5>
+								<p class = "TLP">"FinanceLogo"
+								</p>    
 							</li>
 
 							<li class="event" data-date="1991">
-							<h5>DC</h5>
-							<p class = "TLP">"DCLogo"</p>    
+								<h5>DC</h5>
+								<p class = "TLP">"DCLogo"
+								</p>    
 							</li>
 
 							<li class="event" data-date="1992">
-							<h5>Elders</h5>
-							<p class = "TLP">"EldersLogo"</p>   
+								<h5>Elders</h5>
+								<p class = "TLP">"EldersLogo"
+								</p>   
 							</li>
 
 							<li class="event" data-date="1994">
-							<h5>COR & VM</h5>
-							<p class = "TLP">"COR & VM Logo"</p>   
+								<h5>COR & VM</h5>
+								<p class = "TLP">"COR & VM Logo"
+								</p>   
 							</li>
 
 							<li class="event" data-date="1996">
-							<h5>Youth</h5>
-							<p class = "TLP">"YouthLogo"</p>   
+								<h5>Youth</h5>
+								<p class = "TLP">"YouthLogo"
+								</p>   
 							</li>
 
 							<li class="event" data-date="2002">
-							<h5>CurL, VH, FoC</h5>
-							<p class = "TLP">"CurL, VH, FoC Logo"</p>   
+								<h5>CurL, VH, FoC</h5>
+								<p class = "TLP">"CurL, VH, FoC Logo"
+								</p>   
 							</li>
 
 							<li class="event" data-date="2003">
-							<h5>OVE</h5>
-							<p class = "TLP">"OVELogo"</p>   
+								<h5>OVE</h5>
+								<p class = "TLP">"OVELogo"
+								</p>   
 							</li>
 
 							<li class="event" data-date="2008">
-							<h5>IGC</h5>
-							<p class = "TLP">"IGCLogo"</p>   
+								<h5>IGC</h5>
+								<p class = "TLP">"IGCLogo"
+								</p>   
 							</li>
 
 							<li class="event" data-date="2011">
-							<h5>GO! & PC</h5>
-							<p class = "TLP">"GO!&PCLogo"</p>   
+								<h5>GO! & PC</h5>
+								<p class = "TLP">"GO!&PCLogo"
+								</p>   
 							</li>
 
 							<li class="event" data-date="2012">
-							<h5>OH</h5>
-							<p class = "TLP">"OHLogo"</p>   
+								<h5>OH</h5>
+								<p class = "TLP">"OHLogo"
+								</p>   
 							</li>
 
 							<li class="event" data-date="2013">
-							<h5>IR</h5>
-							<p class = "TLP">"IRLogo"</p>   
+								<h5>IR</h5>
+								<p class = "TLP">"IRLogo"
+								</p>   
 							</li>
 
 							<li class="event" data-date="2017">
-							<h5>MH</h5>
-							<p class = "TLP">"MHLogo"</p>   
+								<h5>MH</h5>
+								<p class = "TLP">"MHLogo"
+								</p>   
 							</li>
-
 						</ul>
-						</div>
-
+					</div>
+					
+					<!--Sub 2_Text-->
 					<h6>Awards & Achievements</h6>
 					<ul class = "ANA_Sub">
-					<li>Nanyang Technological University CoLab4Good Award for Service to Society 2017 - Recipient (Supporting Committee Internal Relations) </li>
+						<li>Nanyang Technological University CoLab4Good Award for Service to Society 2017 - Recipient (Supporting Committee Internal Relations) </li>
 						<li>The President’s Social Service Award 2008 (Informal Category) – Winner!</li>
-						<li>The Singapore Youth Award 2006 (Medal of Commendation) Recipient</li>
-						<li>The Commonwealth Youth Gold Award 2004 Recipient</li>
+						<li>The Singapore Youth Award 2006 (Medal of Commendation) - Recipient</li>
+						<li>The Commonwealth Youth Gold Award 2004 - Recipient</li>
 						<li>Singapore Youth Award 1999 – Community and Youth Services (Team) – Winner!</li>
-						
 					</ul>
+
+					<!--Sub 3_Text--> <!-- Info : target="_blank", open new tab-->
 					<h3>Media featuring WSC</h3>
 					<ul class = "MED_Sub"> 
-						<li>11/07/2018 <a  class = "URLink" href="https://www.zaobao.com.sg/news/fukan/others/story20180711-874239", target="_blank"> <u>Special Project - Challenge ur Limits (南大职业嘉年华 助弱势者规划就业方向)</u></a></li> <!-- target="_blank" open new tab-->
+						<li>11/07/2018 <a  class = "URLink" href="https://www.zaobao.com.sg/news/fukan/others/story20180711-874239", target="_blank"> <u>Special Project - Challenge ur Limits (南大职业嘉年华 助弱势者规划就业方向)</u></a></li> 
 						<li>31/07/2017 <a  class = "URLink" href="http://www.zaobao.com.sg/news/singapore/story20170731-783224", target="_blank"> <u>Regular Service Project - Deaf Community (高声欢唱迎国庆)</u></a></li> 
-						<li>01/10/2016 <a  class = "URLink" href="https://www.zaobao.com.sg/special/report/singapore/gamsia365/story20161001-695686", target="_blank"> <u>WSC's Charity (大学生做慈善　“卖力”洗刷筹款)</u></a></li> 
+						<li>01/10/2016 <a  class = "URLink" href="https://www.zaobao.com.sg/special/report/singapore/gamsia365/story20161001-695686", target="_blank"> <u>WSC - Charity (大学生做慈善　“卖力”洗刷筹款)</u></a></li> 
 					</ul>
 				
-
+					<!--Sub 3_ Images-->
 					<div class="MEDIArow">
 						<div class="MEDIAcolumn">
 							<img  class = "MediaIMG" src="../wp-content/themes/stand-out/assets/img/Hist_Media_Features/2018.17.11.jpg" alt="Image of 2018.17.11" style="width:100%" style="height:300">
@@ -132,83 +155,8 @@ get_header(); ?>
 							<img class = "MediaIMG" src="../wp-content/themes/stand-out/assets/img/Hist_Media_Features/2016.10.01.jpg" alt="Image of 2016.10.01" style="width:100%" style="height:300">
 						</div>
 					</div>
-
-					
-
-				</div>
-
-
-
-				
-				<div class="timeline hide"> <!-- I want to hide the timeline for now because it's lost its aesthetic appeal -->
-					<div class="timeline-content">
-						<div class="timeline-period">1985</div>
-						<div class="timeline-title">ID & PnP</div>
-					</div>
-					
-					<div class="timeline-content">
-						<div class="timeline-period">1987</div>
-						<div class="timeline-title">Finance</div>
-					</div>
-					
-					<div class="timeline-content">
-						<div class="timeline-period">1991</div>
-						<div class="timeline-title">DC</div>
-					</div>
-					
-					<div class="timeline-content">
-						<div class="timeline-period">1992</div>
-						<div class="timeline-title">Elders</div>
-					</div>
-					
-					<div class="timeline-content">
-						<div class="timeline-period">1994</div>
-						<div class="timeline-title">COR & VM</div>
-					</div>
-
-					<div class="timeline-content">
-						<div class="timeline-period">1996</div>
-						<div class="timeline-title">Youth</div>
-					</div>
-
-					<div class="timeline-content">
-						<div class="timeline-period">2002</div>
-						<div class="timeline-title">CURL, VH & FoC</div>
-					</div>
-
-					<div class="timeline-content">
-						<div class="timeline-period">2003</div>
-						<div class="timeline-title">OVE</div>
-					</div>
-
-					<div class="timeline-content">
-						<div class="timeline-period">2008</div>
-						<div class="timeline-title">IGC</div>
-					</div>
-
-					<div class="timeline-content">
-						<div class="timeline-period">2011</div>
-						<div class="timeline-title">GO! & PC</div>
-					</div>
-
-					<div class="timeline-content">
-						<div class="timeline-period">2012</div>
-						<div class="timeline-title">OH</div>
-					</div>
-
-					<div class="timeline-content">
-						<div class="timeline-period">2013</div>
-						<div class="timeline-title">IR</div>
-					</div>
-
-					<div class="timeline-content">
-						<div class="timeline-period">2017</div>
-						<div class="timeline-title">MH</div>
-					</div>
-
-				</div>
+				</div>	
 			</div>
-			<!--</div>-->
 		</div>
 	</main>    
 
