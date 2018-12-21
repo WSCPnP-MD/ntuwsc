a1c35ac69a93dfbf6720d8cab936d1253626d7d6
Tyea <spartashen@hotmail.com>
2018-12-21 18:01:02 +0800

    Content Frame for History almost there.

--- page-14.php
+++ page-14.php
@@ -15,9 +15,7 @@ get_header(); ?>
 
 		<!--Child 0 background frame-->
 		<div class= "overlay">
-			<div class="overlayFN">
 
-			</div>
 		</div>
 
 		<!--Child 1 content frame-->
@@ -28,7 +26,7 @@ get_header(); ?>
 					<!--Sub 0_Text-->
 					<h2> Our History </h2>
 
-					<p class = "Hist_Head"> Welfare Service Club (WSC) had its humble beginnings in 1985 with a few portfolios and to date, we have 18 portfolios consisting of 
+					<p class = "Hist_Head"> Welfare Service Club (WSC) had its humble beginnings in 1985 with a few portfolios and to date, we have 18 portfolios consisting of :
 					</p>
 
 					<ul class = "Hist_Sub">
@@ -41,85 +39,106 @@ get_header(); ?>
 					</p>
 
 					<!--Sub 1_Text_Timeline structure-->
-					<h3>Our Timeline </h3>
+					<h2>Our Timeline </h2>
 					<div id="content">
 
 						<ul class="timeline">
 							<li class="event" data-date="1985">
-								<h5>ID & PNP</h5>
-								<p  class = "TLP">"IDPNPLogo and Description"
+								<h5>PNP (SC)</h5>
+
+								<p  class = "TLP"><img class = "thumbnailIMG"  src="../wp-content/themes/stand-out/assets/img/sc-logos/pnp.jpg" alt="Image of PNP Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption">Publicity and Publication (PNP) publicise the Club’s activities through the boundless realm of the internet and provide technical support in the field of media productions.</span>
+								</p>
+
+								<h5>ID (RSP)</h5>
+
+								<p  class = "TLP"><img class = "thumbnailIMG"  src="../wp-content/themes/stand-out/assets/img/rsp-logos/ID.jpg" alt="Image of ID Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption">Intellectually Disabled (ID) aims to impart useful social skills and proper ethics to our beneficiaries with intellectual disability by organising enriching programmes and outings to develop their potential to the fullest. </span>
 								</p>
 							</li>
 
 							<li class="event" data-date="1987">
-								<h5>Finance</h5>
-								<p class = "TLP">"FinanceLogo"
+								<h5>FINCOMM (SC)</h5>
+								<p class = "TLP"><img class = "thumbnailIMG"  src="../wp-content/themes/stand-out/assets/img/sc-logos/fincomm.jpg" alt="Image of FINCOMM Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption"> Finance Committee (FINCOMM) are responsible for the formulation of budgets, setting standards of reimbursement and preparation of financial statements that reflect a true and fair view.</span>
 								</p>    
 							</li>
 
 							<li class="event" data-date="1991">
-								<h5>DC</h5>
-								<p class = "TLP">"DCLogo"
+								<h5>DC (RSP)</h5>
+								<p class = "TLP"><img class = "thumbnailIMG"  src="../wp-content/themes/stand-out/assets/img/rsp-logos/DC.jpg" alt="Image of DC Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption">Deaf Community (DC) embrace volunteerism as a life-long experience with a focus on helping the Deaf realise their potential</span>
 								</p>    
 							</li>
 
 							<li class="event" data-date="1992">
-								<h5>Elders</h5>
-								<p class = "TLP">"EldersLogo"
+								<h5>Elders (RSP)</h5>
+								<p class = "TLP"><img class = "thumbnailIMG"  src="../wp-content/themes/stand-out/assets/img/rsp-logos/elders.jpg" alt="Image of Elders Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption"> Elders Community seeks to engage the elderly in our society by being a constant source of care and companion for them on a long-term basis. </span>
 								</p>   
 							</li>
 
 							<li class="event" data-date="1994">
-								<h5>COR & VM</h5>
-								<p class = "TLP">"COR & VM Logo"
-								</p>   
+								<h5>COR & VM (SP)</h5>
+
+								<p class = "TLP"><img class = "thumbnailIMG"  src="../wp-content/themes/stand-out/assets/img/sp-logos/cor.jpg" alt="Image of COR Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption">Camp OutReach (COR) aims to assist the Deaf connect with the rest of the hearing world through events and classes.</span> <!-- i simplify this << description need help check is it ok-->
+								</p>
+
+								<p class = "TLP"><img class = "thumbnailIMG" src="../wp-content/themes/stand-out/assets/img/sp-logos/vm.jpg" alt="Image of VM Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption">Volunteer Management (VM) aims to spread the spirit of volunteerism among students of NTU.</span>
+								</p> 
 							</li>
 
 							<li class="event" data-date="1996">
-								<h5>Youth</h5>
-								<p class = "TLP">"YouthLogo"
+								<h5>Youth (RSP)</h5>
+								<p class = "TLP"><img class = "thumbnailIMG"  src="../wp-content/themes/stand-out/assets/img/rsp-logos/youth.jpg" alt="Image of Youth Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption">Youth Community collaborates closely with the Ministry of Social and Family Development (MSF) to inspire troubled teenagers to be a better person.</span>
 								</p>   
 							</li>
 
 							<li class="event" data-date="2002">
-								<h5>CurL, VH, FoC</h5>
-								<p class = "TLP">"CurL, VH, FoC Logo"
+								<h5>CurL (SP)</h5>
+								<p class = "TLP"><img class = "thumbnailIMG"  src="../wp-content/themes/stand-out/assets/img/sp-logos/curl.jpg" alt="Image of CurL Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption">Challenge ur Limits (CurL) create opportunities for beneficiaries and volunteers to interact and participate in exciting activities which will challenge both their mental and physical limits. </span>
+								</p> 
+
+								<h5>VH & FoC (RSP)</h5>
+								<p class = "TLP"><img class = "thumbnailIMG"  src="../wp-content/themes/stand-out/assets/img/rsp-logos/vh.jpg" alt="Image of VH Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption">Visually Handicapped (VH) providing befriending and tuition services to Visually Handicapped beneficiaries from all walks of life.</span>
+								</p> 
+
+								<p class = "TLP"><img class = "thumbnailIMG"  src="../wp-content/themes/stand-out/assets/img/rsp-logos/FOC.jpg" alt="Image of FoC Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption">Friends of Children (FoC) provides weekly mentoring sessions and life skills sessions for children (4 to 17 years old) from lower income families.</span>
 								</p>   
 							</li>
 
 							<li class="event" data-date="2003">
-								<h5>OVE</h5>
-								<p class = "TLP">"OVELogo"
+								<h5>OVE(SP)</h5>
+								<p class = "TLP"><img class = "thumbnailIMG"  src="../wp-content/themes/stand-out/assets/img/sp-logos/ove.jpg" alt="Image of OVE Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption">Overseas Volunteering Expedition (OVE) aims to provide individuals with the passion for volunteering a platform that establishes an opportunity to benefit less-privileged communities beyond geographical boundaries.</span>
 								</p>   
 							</li>
 
 							<li class="event" data-date="2008">
-								<h5>IGC</h5>
-								<p class = "TLP">"IGCLogo"
+								<h5>IGC (SP)</h5>
+								<p class = "TLP"><img class = "thumbnailIMG"  src="../wp-content/themes/stand-out/assets/img/sp-logos/igc.jpg" alt="Image of IGC Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption">Ignite Change (IGC) is an exciting freshmen orientation camp held yearly that aims to foster bonds amongst the participants, as well as igniting the spirit of volunteerism within potential volunteers.</span>
 								</p>   
 							</li>
 
 							<li class="event" data-date="2011">
-								<h5>GO! & PC</h5>
-								<p class = "TLP">"GO!&PCLogo"
-								</p>   
+								<h5>GO (SC)</h5>
+								<p class = "TLP"><img class = "thumbnailIMG"  src="../wp-content/themes/stand-out/assets/img/sc-logos/go.png" alt="Image of GO! Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption">Growth & Opportunities (GO) organizing annual events for the entire NTU WSC and seeking sponsorship to support our volunteering activities.</span>
+								</p>
+
+								<h5>PC (RSP)</h5>
+								<p class = "TLP"><img class = "thumbnailIMG"  src="../wp-content/themes/stand-out/assets/img/rsp-logos/pc.jpg" alt="Image of PC Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption">Physically Challenged (PC) bridge the existing gap between the society and the community of the physically challenged, allowing them to be A PART of the society and not APART from the society.</span>
+								</p>    
 							</li>
 
 							<li class="event" data-date="2012">
-								<h5>OH</h5>
-								<p class = "TLP">"OHLogo"
+								<h5>OH (RSP)</h5>
+								<p class = "TLP"><img class = "thumbnailIMG"  src="../wp-content/themes/stand-out/assets/img/rsp-logos/oh.jpg" alt="Image of OH Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption">Operation Hope (OH) reach out and connect to children and youths suffering from chronic and life-threatening diseases, as well as raise awareness on the plight of these beneficiaries.</span>
 								</p>   
 							</li>
 
 							<li class="event" data-date="2013">
-								<h5>IR</h5>
-								<p class = "TLP">"IRLogo"
+								<h5>IR (SC)</h5>
+								<p class = "TLP"><img class = "thumbnailIMG"  src="../wp-content/themes/stand-out/assets/img/sc-logos/ir.jpg" alt="Image of IR Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption">Internal Relations (IR) organise large scale charity and bonding events for the entire Welfare Services Club.</span>
 								</p>   
 							</li>
 
 							<li class="event" data-date="2017">
-								<h5>MH</h5>
-								<p class = "TLP">"MHLogo"
+								<h5>MH (RSP)</h5>
+								<p class = "TLP"><img class = "thumbnailIMG"  src="../wp-content/themes/stand-out/assets/img/rsp-logos/mh.jpg" alt="Image of MH Logo" style = "width:100px;height:100px;margin-right:15px"><span class="caption">Mental Health (MH) aims to create greater awareness of our beneficiaries and to help students understand them better.</span>
 								</p>   
 							</li>
 						</ul>
@@ -138,8 +157,8 @@ get_header(); ?>
 					<!--Sub 3_Text--> <!-- Info : target="_blank", open new tab-->
 					<h3>Media featuring WSC</h3>
 					<ul class = "MED_Sub"> 
-						<li>11/07/2018 <a  class = "URLink" href="https://www.zaobao.com.sg/news/fukan/others/story20180711-874239", target="_blank"> <u>Special Project - Challenge ur Limits (南大职业嘉年华 助弱势者规划就业方向)</u></a></li> 
-						<li>31/07/2017 <a  class = "URLink" href="http://www.zaobao.com.sg/news/singapore/story20170731-783224", target="_blank"> <u>Regular Service Project - Deaf Community (高声欢唱迎国庆)</u></a></li> 
+						<li>11/07/2018 <a  class = "URLink" href="https://www.zaobao.com.sg/news/fukan/others/story20180711-874239", target="_blank"> <u>Special Project(SP) - Challenge your limits (南大职业嘉年华 助弱势者规划就业方向)</u></a></li> 
+						<li>31/07/2017 <a  class = "URLink" href="http://www.zaobao.com.sg/news/singapore/story20170731-783224", target="_blank"> <u>Regular Service Project(RSP) - Deaf Community (高声欢唱迎国庆)</u></a></li> 
 						<li>01/10/2016 <a  class = "URLink" href="https://www.zaobao.com.sg/special/report/singapore/gamsia365/story20161001-695686", target="_blank"> <u>WSC - Charity (大学生做慈善　“卖力”洗刷筹款)</u></a></li> 
 					</ul>
 				
